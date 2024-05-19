eb4a8b1a779d02a81e5132460df1fc2b752c5fe8
= <=>
2024-05-19 18:20:08 -0300

    base tratada

--- tratamiento_de_datos.R
+++ tratamiento_de_datos.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# Tratamiento de datos para empresas
+# Tratamiento de datos para empresas ####
 load(file = "empresas_totales.RData")
 
 # Se parte de datos enlistados por fila. Cada fila es una empresa. Algunas tienen web y otras no.
@@ -23,3 +23,23 @@ empresas_uniformes <- lista_separada %>%
 tibble_data <- empresas_uniformes %>%
   map_df(~ set_names(., paste0("Col", 1:max_length)))
 
+# Filtramos todas las filas de páginas
+df <- tibble_data %>% 
+  filter(!grepl("Página", Col1))
+
+# En la columna descripción eliminamos la palabra y los paréntesis
+df$Col2 <- gsub("Descripción ", "", df$Col2) 
+df$Col2 <- gsub("\\(", "", df$Col2) 
+df$Col2 <- gsub("\\) ", "", df$Col2) 
+
+# La columna 5 es la columna adonde se encuentra el parque. La completamos
+df$Col5 <- if_else(grepl("Web", df$Col3) == TRUE, df$Col5, df$Col4)
+
+# Ahora si podemos eliminar la palabra web
+df$Col3 <- gsub("Web ", "", df$Col3)
+df$Col3 <- if_else(grepl("Radicada", df$Col3) == TRUE, NA, df$Col3)
+
+# La columna cuatro no sirve
+df$Col4 <- NULL
+
+# Tratamiento para datos de parques ####
